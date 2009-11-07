ae1513b8e4be3a96927b3cb16103e52ab76cff74
Alexander Polakov <polachok@gmail.com>
2009-11-07 21:35:05 +0300

    Add debug flags

--- config.mk
+++ config.mk
@@ -11,14 +11,14 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC} `pkg-config --cflags xft` `pkg-config --cflags fuse`
-LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 `pkg-config --libs xft` `pkg-config --libs fuse`
+INCS = -I. -I/usr/include -I${X11INC}  `pkg-config --cflags fuse`
+LIBS = -L/usr/lib -lc -L${X11LIB} -lX11 `pkg-config --libs fuse`
 
 # flags
 CFLAGS = -Os ${INCS} -DVERSION=\"${VERSION}\" 
 LDFLAGS = -s ${LIBS}
-#CFLAGS = -g -std=c99 -pedantic -Wall -O2 ${INCS} -DVERSION=\"${VERSION}\"
-#LDFLAGS = -g ${LIBS}
+CFLAGS = -g -Wall -O0 ${INCS} -DVERSION=\"${VERSION}\"
+LDFLAGS = -g ${LIBS}
 
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
